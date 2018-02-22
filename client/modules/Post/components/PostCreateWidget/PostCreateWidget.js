43e754f4fa2725b902f86684f853e82d2fef8a15
Adrian Rak <adrrak@tlen.pl>
2018-02-22 08:26:38 +0100

    localStorage

--- client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -10,6 +10,25 @@ export class PostCreateWidget extends Component {
     const titleRef = this.refs.title;
     const contentRef = this.refs.content;
     const votes = 0;
+
+    let local = localStorage.setItem('contentRef', this.refs.content.value);
+  
+    
+ 
+    //localStorage.getItem('contentRef');
+ 
+    
+    console.log(nameRef.value);
+    console.log(titleRef.value);
+    console.log(contentRef.value);
+    console.log(window.localStorage);
+
+    // setInterval(function() {
+    //   localStorage.setItem('contentRef', this.refs.content.value);
+    // }, 1000);
+   let myLocal = localStorage.getItem('contentRef');
+  
+
     if (nameRef.value && titleRef.value && contentRef.value) {
       this.props.addPost(nameRef.value, titleRef.value, contentRef.value, votes);
       nameRef.value = titleRef.value = contentRef.value = '';
@@ -24,7 +43,7 @@ export class PostCreateWidget extends Component {
           <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
           <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" />
           <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" />
-          <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content" />
+          <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content">{localStorage.getItem('contentRef')}</textarea>
           <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
         </div>
       </div>
