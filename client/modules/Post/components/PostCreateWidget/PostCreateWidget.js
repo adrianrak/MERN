a2bf67b5b2b61a0ce03e8fd61dbf6c3a07e1ae5c
Adrian Rak <adrrak@tlen.pl>
2018-02-22 12:38:55 +0100

    correction localStorage

--- client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -5,35 +5,40 @@ import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
 import styles from './PostCreateWidget.css';
 
 export class PostCreateWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      localData: '',
+    };
+  }
+
   addPost = () => {
     const nameRef = this.refs.name;
     const titleRef = this.refs.title;
     const contentRef = this.refs.content;
     const votes = 0;
 
-    let local = localStorage.setItem('contentRef', this.refs.content.value);
-  
-    
- 
-    //localStorage.getItem('contentRef');
- 
-    
-    console.log(nameRef.value);
-    console.log(titleRef.value);
-    console.log(contentRef.value);
-    console.log(window.localStorage);
-
-    // setInterval(function() {
-    //   localStorage.setItem('contentRef', this.refs.content.value);
-    // }, 1000);
-   let myLocal = localStorage.getItem('contentRef');
-  
-
-    if (nameRef.value && titleRef.value && contentRef.value) {
-      this.props.addPost(nameRef.value, titleRef.value, contentRef.value, votes);
-      nameRef.value = titleRef.value = contentRef.value = '';
+    if (nameRef.value && titleRef.value) {
+      this.props.addPost(nameRef.value, titleRef.value, this.state.localData, votes);
+      nameRef.value = titleRef.value = this.state.localData = '';
+      localStorage.removeItem('contentRef');
     }
   };
+
+  
+  componentDidMount() {
+    const localStorageData = localStorage.getItem('contentRef');
+    this.setState({
+      localData: localStorageData || '',
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      localData: e.target.value,
+    });
+    localStorage.setItem('contentRef', e.target.value);
+  }
 
   render() {
     const cls = `${styles.form} ${(this.props.showAddPost ? styles.appear : '')}`;
@@ -43,7 +48,7 @@ export class PostCreateWidget extends Component {
           <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
           <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" />
           <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" />
-          <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content">{localStorage.getItem('contentRef')}</textarea>
+          <textarea onChange={this.handleChange} value={this.state.localData} placeholder={this.props.intl.messages.postContent} className={styles['form-field']}></textarea>
           <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
         </div>
       </div>
